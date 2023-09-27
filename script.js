557e1d9bf72d1e63f1aa5be97a39daa9b332ed75
kmhchess <kmhchess@gmail.com>
2023-09-26 22:15:41 -0500

    Update for lab 3
    Add animation

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
     $('#foods tbody tr:even').css('background-color','#dddddd');
+    $("#feedback").text("Click the button if you choose.");
 
     $('#toggleButton').click(function(){
-        $('#high_GI').toggle();
-        
+        $('#high_GI').toggle();        
         if($('#high_GI').is(':visible')) {
           $(this).val('Hide High GI Foods');
+          $("#feedback").animate({right: '+=250px'}, 500, 'swing');
+          $("#feedback").text("Click the button if you choose.");
         } else {
           $(this).val('Show High GI Foods');
+          $("#feedback").animate({right: '-=250px'}, 500, 'swing');
+          $("#feedback").text("Wise Choice!");
         }
     });
 
